63853114e78171e77f880358993069007f517c1f
chaicham <chai-chaim.magal@amdocs.com>
2022-05-16 11:49:01 +0300

    change values of answres

--- src/utils/calculations.js
+++ src/utils/calculations.js
@@ -21,11 +21,11 @@ function genRandomNumber(maxValue) {
 }
 
 function changeArray(currectAnswer) {
-  MainArray[0].value = genRandomNumber(maxValue);
+  MainArray[0].value = genRandomNumber(maxValue * 2).toFixed(1);
   MainArray[0].isCorrect = false;
-  MainArray[1].value = genRandomNumber(maxValue);
+  MainArray[1].value = genRandomNumber(maxValue * 2).toFixed(1);
   MainArray[1].isCorrect = false;
-  MainArray[2].value = currectAnswer;
+  MainArray[2].value = currectAnswer.toFixed(1);
   MainArray[2].isCorrect = true;
 
   shuffleArray(MainArray);
